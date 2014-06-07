a297429bdb4c30403100416860a6d21fb0f0254a
Juan Francisco Cardona Mc'Cormick <jfcmacroeafit@gmail.com>
2014-06-07 15:01:24 -0500

    Deleting SincronizadorImpl.java

--- src/co/edu/eafit/dis/st0257/concurrencia/lavdual/Lavadora.java
+++ src/co/edu/eafit/dis/st0257/concurrencia/lavdual/Lavadora.java
@@ -1,35 +1,44 @@
 package co.edu.eafit.dis.st0257.concurrencia.lavdual;
 
 import java.util.Random;
+
 /**
  * Esta clase simula el comportamiento de una de las lavadoras
  * que van a trabajar de manera dual.
  */
 public class Lavadora implements Runnable {
 
+    // Agente de sincronización
+    private Sincronizador agenteSin;
+    private GenCargas genCargas;
+    private Random random;
 
-   // Agente de sincronización
-   private Sincronizador agenteSin;
-   private GenCargas genCargas;
-   private Random random;
+    public Lavadora(Sincronizador agenteSin,
+		    GenCargas genCargas) {
 
-   public Lavadora(Sincronizador agenteSin,
-                   GenCargas genCargas) {
+	this.agenteSin = agenteSin;
+	this.genCargas = genCargas;
+    }
 
-      this.agenteSin = agenteSin;
-      this.genCargas = genCargas;
-   }
+    /**
+     * lavar - se encarga de realizar el lavado
+     */
+    public void lavar() {
+    }
 
-   public void lavar() {
-   }
+    /**
+     *run se encarga de ejecutar el proyecto
+     */
+    public void run() {
 
-   public void run() {
-      int cap = 0;
-      while (true) {
-         cap = genCargas.obtenerSigCarga();
-         agenteSin.arrancar(cap);
-         lavar();
-         agenteSin.parar(cap);
-      }
-   }
+	int cap = 0;
+
+	while (true) {
+
+	    cap = genCargas.obtenerSigCarga();
+	    agenteSin.arrancar(cap);
+	    lavar();
+	    agenteSin.parar(cap);
+	}
+    }
 }
--- src/co/edu/eafit/dis/st0257/concurrencia/lavdual/Sincronizador.java
+++ src/co/edu/eafit/dis/st0257/concurrencia/lavdual/Sincronizador.java
@@ -2,6 +2,18 @@ package co.edu.eafit.dis.st0257.concurrencia.lavdual;
 
 public interface Sincronizador {
 
+    /**
+     * arracar - se encarga de iniciar la ejecución de una
+     *           lavadora.
+     * @param cap determina la capacidad inicial de la lavadora.
+     */
     public void arrancar(int cap);
+
+
+    /**
+     * parar - se encarga de parar la ejecución de una 
+     *         lavadora.
+     * @param cap determina la capacidad de terminar la lavadora.
+     */
     public void parar(int cap);
 }
